f26dc62a78f423022c69e0201d0ebe5dd8fc342e
Yanko Simeonoff <ysimeonov@despark.com>
2022-10-12 23:55:18 +0300

    dep: add symfony/ux-live-component

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
---
+++ src/Service/StationService.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Doctrine\DBAL\Result;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * Class StationService
+ */
+class StationService
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em
+    )
+    {
+    }
+
+    public function findCurrent(): Result
+    {
+        $sql = /** @lang PostgreSQL */
+            <<<SQL
+            select s.station_id, s.name, sd.*
+            from station s
+              inner join lateral (
+                  select * 
+                  from station_data sd 
+                  where sd.station_id=s.station_id 
+                  order by sd.created_at desc 
+                  limit 1
+              ) sd on true
+        SQL;
+
+        $stmt = $this->em->getConnection()->prepare($sql);
+
+        return $stmt->executeQuery([]);
+    }
+}
